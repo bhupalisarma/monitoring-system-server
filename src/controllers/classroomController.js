3ea132ce70a49ac702543df61cca51fcf7874b4a
Bhupali Sarma <85697658+bhupalisarma@users.noreply.github.com>
2023-07-26 14:38:46 +0530

    Post schema added

--- src/controllers/classroomController.js
+++ src/controllers/classroomController.js
@@ -5,8 +5,7 @@ const Classroom = require('../models/Classroom');
 // Get all classrooms created by the logged-in mentor
 const getAllClassrooms = async (req, res) => {
     try {
-        const mentorId = req.user._id;
-        const classrooms = await Classroom.find({ mentor: mentorId });
+        const classrooms = await Classroom.find();
         res.json(classrooms);
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch classrooms' });
@@ -35,7 +34,42 @@ const createClassroom = async (req, res) => {
     }
 };
 
+// Create a post in a classroom
+const createPost = async (req, res) => {
+    try {
+        const { heading, content, files } = req.body;
+        const { classroomId } = req.params;
+
+        // Find the classroom by ID
+        const classroom = await Classroom.findById(classroomId);
+
+        if (!classroom) {
+            return res.status(404).json({ error: 'Classroom not found' });
+        }
+
+        // Create a new post
+        const newPost = {
+            heading,
+            content,
+            files,
+            createdAt: new Date(),
+            comments: [],
+        };
+
+        // Add the new post to the classroom
+        classroom.posts.push(newPost);
+
+        // Save the updated classroom to the database
+        const updatedClassroom = await classroom.save();
+
+        res.json(updatedClassroom);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to create post' });
+    }
+};
+
 module.exports = {
     getAllClassrooms,
     createClassroom,
+    createPost
 };
